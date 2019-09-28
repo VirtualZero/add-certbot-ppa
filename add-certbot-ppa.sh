4b1c561b1138016560e5b62eb837ddf8e4e44752
Ubuntu <ubuntu@ip-172-31-26-83.us-east-2.compute.internal>
2019-09-28 15:45:28 +0000

    Added -y arg to install

--- add-certbot-ppa.sh
+++ add-certbot-ppa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
